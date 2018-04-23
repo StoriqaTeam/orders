ae772c5ef4af9a75a001cb165ef5b6098d455b82
Artem Vorotnikov <artem@vorotnikov.me>
2018-04-23 16:23:49 +0300

    Better typing

--- src/models/cart.rs
+++ src/models/cart.rs
@@ -2,9 +2,11 @@ use std::collections::HashMap;
 
 pub struct CartItemInfo {}
 
+pub type CartProducts = HashMap<i32, i32>;
+
 #[derive(Clone, Debug, Default, PartialEq, Serialize, Deserialize)]
 pub struct Cart {
-    pub products: HashMap<i32, i32>,
+    pub products: CartProducts,
 }
 
 #[derive(Clone, Debug, Default, PartialEq, Serialize, Deserialize)]
