03e5ee11951e32005b8c6b83efdd546c97b1bddc
Artem Vorotnikov <artem@vorotnikov.me>
2018-06-25 21:59:17 +0300

    Migration fix

--- migrations/2018-06-14-000000_create_order_diffs/up.sql
+++ migrations/2018-06-14-000000_create_order_diffs/up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE order_diffs (
     id        order_diff_id PRIMARY KEY DEFAULT uuid_generate_v4(),
-    parent    order_id      NOT NULL,
+    parent    order_id      NOT NULL REFERENCES orders (id) ON DELETE CASCADE,
     committer user_id       NOT NULL,
     datetime  DATE          NOT NULL,
     status    VARCHAR       NOT NULL,
